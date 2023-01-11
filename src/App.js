ffaa323827c83afc4df4ecc1a52b865c34712a03
puppe1990 <matheus.puppe90@hotmail.com>
2023-01-11 19:53:29 -0300

    using letter with sound

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import React, { useState } from 'react';
 import './styles.css';
 
+const AudioLetter = ({ letter, handleLetterClick }) => {
+  const [audio, setAudio] = React.useState(null);
+
+  React.useEffect(() => {
+      import(`./audio/${letter}.mp3`)
+          .then(audioModule => setAudio(audioModule.default))
+  }, [letter]);
+
+  const handleClick = () => {
+      const a = new Audio(audio);
+      a.load();
+      a.play();
+      handleLetterClick(letter)
+  }
+
+  return (
+      <button className="alphabet-letter" onClick={handleClick}>
+          {letter}
+      </button>
+  );
+}
+
 function App() {
   const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   const [selectedLetter, setSelectedLetter] = useState(alphabet[0]);
 
   const handleLetterClick = (letter) => {
-    setSelectedLetter(letter);
-    playSound(letter);
-  };
-
-  const playSound = (letter) => {
-    const audio = new Audio(`http://soundfxcenter.com/human/speech/8d82b5_Letter_${letter}_Sound_Effect.mp3`);
-    audio.load();
-    audio.play();
+      setSelectedLetter(letter);
   };
 
   return (
-    <div className="container">
-      <div className="card-center">
-        <h1 className="selected-letter">{selectedLetter}</h1>
+      <div className="container">
+          <div className="card-center">
+              <h1 className="selected-letter">{selectedLetter}</h1>
+          </div>
+          <div className="alphabet-container">
+              {alphabet.map(letter => <AudioLetter letter={letter} handleLetterClick={handleLetterClick} key={letter} />)}
+          </div>
       </div>
-      <div className="alphabet-container">
-        {alphabet.map((letter) => (
-          <button className="alphabet-letter" onClick={() => handleLetterClick(letter)}>
-            {letter}
-          </button>
-        ))}
-      </div>
-    </div>
   );
 }
 
