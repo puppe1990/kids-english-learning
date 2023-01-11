f0d1c6f82dfcc5f4845acb9f4a7354350dc9c130
puppe1990 <matheus.puppe90@hotmail.com>
2023-01-11 15:08:35 -0300

    numbers container

---
+++ src/Number.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './styles.css';
+
+function Number() {
+  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  const [selectedNumber, setSelectedNumber] = useState(numbers[0]);
+
+  const handleNumberClick = (number) => {
+    setSelectedNumber(number);
+  };
+
+  return (
+    <div className="container">
+      <div className="card-center">
+        <h1 className="selected-number">{selectedNumber}</h1>
+      </div>
+      <div className="number-container">
+        {numbers.map((number) => (
+          <button className="order-number" onClick={() => handleNumberClick(number)}>
+            {number}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Number;
